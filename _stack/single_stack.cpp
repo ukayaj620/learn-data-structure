dfe0d3ddbcaded6ff6867db36f0e49313866a810
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2019-12-09 20:03:49 +0700

    add single stack code

--- _stack/single_stack.cpp
+++ _stack/single_stack.cpp
@@ -1,9 +1,86 @@
 #include <iostream>
+#define MAX 9999
 
 using namespace std;
 
+void push(int);
+void pop();
+void printStack();
+
+int stack[MAX];
+int top = -1;
+
 int main()
 {
-	cout << "Hello, World!\n";
+	int pil;
+	do
+	{
+		cout << "SINGLE STACK\n"
+			<< "1. PUSH\n2. POP\n3. PRINT\n4. EXIT\n"
+			<< "Your choice: "; 
+		cin >> pil;
+		
+		switch(pil)
+		{
+			case 1: {
+				int num;
+				cout << "Input a number: "; cin >> num;
+				push(num);
+				break;
+			}
+			case 2: {
+				pop();
+				break;
+			}
+			case 3: {
+				printStack();
+				break;
+			}
+			default: {
+				cout << "Wrong input!\n";
+				break;
+			}
+		}
+		
+		system("PAUSE");
+		
+	}while (pil != 4);
 	return 0;
+}
+
+void push(int num)
+{
+	if (top == MAX) 
+	{
+		cout << "\nSTACK OVERFLOW!\n";
+		return;
+	} 
+	else
+	{
+		stack[++top] = num;
+		cout << num << " has been pushed into stack!\n";
+	}
+}
+
+void pop()
+{
+	if (top == -1)
+	{
+		cout << "STACK UNDERFLOW!\n";
+		return;
+	}
+	else
+	{
+		cout << stack[top--] << " has been poped out from stack!\n";
+	}
+}
+
+void printStack()
+{
+	int n = top + 1;
+	cout << "\nSTACK POOL:\n";
+	while(n--)
+	{
+		cout << stack[n] << "\n";
+	}
 }
