56239b393d47115a1918a4434c7fd913b15101ec
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2019-12-24 22:08:20 +0700

    create double ended queue

---
+++ _queue/double_ended_queue.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 9999
+
+void enqueue_left(int);
+void enqueue_right(int);
+void dequeue_left();
+void dequeue_right();
+void printQueue();
+
+int queue[MAX];
+int left = -1;
+int right = -1;
+
+int main ()
+{
+	
+	return 0;
+}
+
+void enqueue_left(int)
+{
+	
+}
+
+void enqueue_right(int)
+{
+	
+}
+
+void dequeue_left()
+{
+	
+}
+
+void dequeue_right()
+{
+	
+}
+
+void printQueue()
+{
+	
+}
