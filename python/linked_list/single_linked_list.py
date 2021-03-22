4d1319f7f1343e1e0bb80972e53cdff125c75ab6
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-03-22 13:18:21 +0700

    feat: add basic operations of single linked list

--- python/linked_list/single_linked_list.py
+++ python/linked_list/single_linked_list.py
@@ -21,6 +21,43 @@ class SinglyLinkedList:
     self.tail = node
 
 
+  def insert_after(self, find, data):
+    node = Node(data)
+    _, find_node = self.search(find)
+    if find_node is not None:
+      node.next = find_node.next
+      find_node.next = node
+
+      if find_node is self.tail:
+        self.tail = node
+
+
+  def delete(self, find):
+    prev_find_node, find_node = self.search(find)
+    if find_node is not None:
+      if find_node is self.head:
+        self.head = self.head.next
+      elif find_node is self.tail:
+        prev_find_node.next = None
+        self.tail = prev_find_node
+      else:
+        prev_find_node.next = find_node.next
+        
+
+  def search(self, find):
+    node = self.head
+    node_prev = None
+    while node is not None:
+      if node.data == find:
+        return node_prev, node
+
+      node_prev = node
+      node = node.next
+    
+    print("No data found!")
+    return None
+
+
   def length(self):
     count = 0
     node = self.head
@@ -33,17 +70,34 @@ class SinglyLinkedList:
 
   def print(self):
     node = self.head
-    while node:
-      print(node.data, end=" ")
-      node = node.next
+    if node is None:
+      print("Linked list is empty!")
+    else:
+      print("\nHead: {}".format(self.head.data))
+      print("Tail: {}".format(self.tail.data))
+      print("Length: {}".format(self.length()))
+      print("Data:", end=" ")
+      while node:
+        print(node.data, end=" -> " if node.next is not None else "\n")
+        node = node.next
 
 
 if __name__=='__main__': 
   singlyLinkedList = SinglyLinkedList()
-  while True:
-    data = input()
-    if data == "":
-      break
-    singlyLinkedList.insert_node(int(data))
+  
+  singlyLinkedList.insert_node(100)
+  singlyLinkedList.insert_node(-10)
+  singlyLinkedList.insert_node(90)
+  singlyLinkedList.insert_after(100, 10)
+  singlyLinkedList.insert_after(90, 40)
+  singlyLinkedList.insert_after(40, -80)
+  singlyLinkedList.print()
 
-singlyLinkedList.print()
+  singlyLinkedList.delete(100)
+  singlyLinkedList.print()
+
+  singlyLinkedList.delete(-80)
+  singlyLinkedList.print()
+
+  singlyLinkedList.delete(10)
+  singlyLinkedList.print()
